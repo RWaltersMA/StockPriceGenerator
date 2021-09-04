65cf71a01f6d311e8c68d8afc52ab1650c33f91a
Rob Walters <Robert.Walters@mongodb.com>
2021-09-04 19:34:04 -0400

    updated to create time series collection

--- stockgen.py
+++ stockgen.py
@@ -1,16 +1,12 @@
+from os import error
 import random
-from random import seed
 from random import randint
-import csv
 import argparse
-import pymongo
-from pymongo import errors
-
-from datetime import date, timedelta, datetime as dt
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from datetime import timedelta, datetime as dt
 import threading
 import sys
-import json
-import os
 import time
 
 lock = threading.Lock()
@@ -23,7 +19,6 @@ company_name=[]
 
 #the following two functions are used to come up with some fake stock securities
 def generate_symbol(a,n,e):
-    #we need to break this out into its own function to do checks to make sure we dont have duplicate symbols
     for x in range(1,len(a)):
         symbol=str(a[:x]+n[:1]+e[:1])
         if symbol not in company_symbol:
@@ -54,34 +49,31 @@ def getvalue(old_value):
         new_value = old_value - change_amount
     return round(new_value, 2)
 
-#When the conainters launch, we want to not crash out if the MongoDB Server container isn't fully up yet so here we wait until we can connect
 def checkmongodbconnection():
     try:
-        c = pymongo.MongoClient(MONGO_URI)
+        c=MongoClient(MONGO_URI, server_api=ServerApi('1'), serverSelectionTimeoutMS=5000)
         c.admin.command('ismaster')
         time.sleep(2)
         c.close()
         return True
-    except pymongo.errors.ServerSelectionTimeoutError as e:
-        print('Could not connect to server: %s',e)
-        return False
-    else:
-        c.close()
+    except:
+        print('\nCould not connect to MongoDB.\n\n')
         return False
 
 def main():
     global args
     global MONGO_URI
-    # capture parameters from the command line
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-s","--symbols", type=int, default=5, help="number of financial stock symbols")
-    #MongoDB Connector for Apache Kafka requires the source to be a replica set
     parser.add_argument("-c","--connection",type=str, default='mongodb://127.0.0.1', help="MongoDB connection string")
     parser.add_argument("-d","--database",type=str, default='StockData', help="Name of destination database (default Stock)")	
-    parser.add_argument("-col","--collection",type=str, default='StockData', help="Name of destination collection (default StockData)")
+    parser.add_argument("-col","--collection",type=str, default='Stocks', help="Name of destination collection (default StockData)")
     parser.add_argument("-x","--duration",type=int, default=0, help="Number of minutes of data to generate (default 0 - forever)")
-	
-	
+    parser.add_argument("-drop","--drop",help="Clears the destination collection of data before starting",action="store_true")
+    parser.add_argument("-ts","--timeseries",help="Creates the collection as a timeseries collection",action="store_true")
+    parser.add_argument("-as","--AsString",help="Write the tx time as a string",action="store_true")
+
     args = parser.parse_args()
 
     if args.symbols:
@@ -93,6 +85,9 @@ def main():
     threads = []
 
     generate_securities(args.symbols)
+
+    if args.AsString & args.timeseries:
+      print('\n** Time series collections require timeseries field to be in datetime field, ignoring --AsString parameter **\n\n')
 
     for i in range(0, 1): # parallel threads
         t = threading.Thread(target=worker, args=[int(i), int(args.symbols)])
@@ -119,11 +114,25 @@ def worker(workerthread, numofsymbols):
                 break
         print('Successfully connected to MongoDB')
 
-        c = pymongo.MongoClient(MONGO_URI)
-        db = c.get_database(name='Stocks')
+        c = MongoClient(MONGO_URI, server_api=ServerApi("1", strict=False))
+        db = c.get_database(name=args.database)
         txtime = dt.now()
         txtime_end=txtime+timedelta(minutes=args.duration)
-        print('Start time ' + txtime.strftime('%Y-%m-%d %H:%M:%S') + '\nEnd time  ' + txtime_end.strftime('%Y-%m-%d %H:%M:%S'))
+        if args.drop:
+            print('\nDropping collection ' + args.collection + '\n')
+            db.drop_collection(args.collection)
+        if args.timeseries:
+            collection = db.create_collection(name=args.collection,timeseries= {"timeField": "tx_time", "granularity": "seconds"})
+            print('Create collection result=' + collection.full_name)
+        print('Data Generation Summary:\n{:<12} {:<12}\n{:<12} {:<12}\n{:<12} {:<12}'.format('# symbols',args.symbols,'Database',args.database,'Collection',args.collection))
+        print('\n{:<8}  {:<50}'.format('Symbol','Company Name'))
+        for x in range(len(company_name)):
+          print('{:<8}  {:<50}'.format(company_symbol[x],company_name[x]))
+        print('\n{:<12} {:<12}'.format('Start time',txtime.strftime('%Y-%m-%d %H:%M:%S')))
+        if args.duration:
+            print('{:<12} {:<12}\n'.format('End time',txtime_end.strftime('%Y-%m-%d %H:%M:%S')))
+        else:
+            print('No end time - run until user stop (control-Z)\n\n')
         counter=0
         bContinue=True
         while bContinue:
@@ -132,23 +141,28 @@ def worker(workerthread, numofsymbols):
                 x = getvalue(last_value[i])
                 last_value[i] = x
                 try:
-                    #For now we are writing a flat schema model (one document per data point) since the demo focus is the data flow through Kafka
-                    #For performance you would want to use a fixed based schema for time-series data in MongoDB
-                    result=db['StockData'].insert_one( {'company_symbol' : company_symbol[i], 'company_name': company_name[i],'price': x, 'tx_time': txtime.strftime('%Y-%m-%dT%H:%M:%SZ')}) # because mysql stores strings txtime})
+                    if args.AsString:
+                      result=db[args.collection].insert_one( {'company_symbol' : company_symbol[i], 'company_name': company_name[i],'price': x, 'tx_time': txtime.strftime('%Y-%m-%dT%H:%M:%SZ')})
+                    else:
+                      result=db[args.collection].insert_one( {'company_symbol' : company_symbol[i], 'company_name': company_name[i],'price': x, 'tx_time': txtime})
                     counter=counter+1
                     if counter%100==0:
-                      print('Wrote ' + str(counter) + ' samples, last one ->  ' + company_symbol[i] + ' ' + company_name[i] + ' traded at ' + str(x) + ' ' + txtime.strftime('%Y-%m-%d %H:%M:%S'))
+                      if args.duration>0:
+                        print('Generated ' + str(counter) + ' samples ({0:.0%})'.format(counter/(numofsymbols*args.duration*60)))
+                      else:
+                        print('Generated ' + str(counter))
                     if args.duration>0:
                       if txtime > txtime_end:
-                        print('Finished')
                         bContinue=False
                         continue
                 except Exception as e:
                     print("error: " + str(e))
             txtime+=timedelta(seconds=1)
+        duration=txtime - dt.now()
+        print('\nFinished - ' + str(duration).split('.')[0])
     except:
         print('Unexpected error:', sys.exc_info()[0])
         raise
 
-
-main()
+if __name__ == '__main__':
+    main()
